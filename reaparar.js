e9de7a6e27109b3c29add5f51851eae7d28177f6
jhonye73 <jespitia@umariana.edu.co>
2025-05-12 18:00:54 -0500

    reaparar.js

--- reaparar.js
+++ reaparar.js
@@ -35,7 +35,52 @@ app.post("/", async (req, res) => {
     to: correos.join(","),
     subject: subject || "Sin asunto",
     text: text || "Sin mensaje",
-    html: "<p><strong>Hola</strong>, este es un <em>correo masivo</em> con Nodemailer usando <code>import</code>.</p>",
+    html: "<html>
+  <head>
+    <meta charset="UTF-8" />
+    <style>
+      body {
+        font-family: Arial, sans-serif;
+        color: #333;
+        line-height: 1.5;
+      }
+      .footer {
+        margin-top: 30px;
+        font-size: 12px;
+        color: #666;
+      }
+      .emphasis {
+        font-weight: bold;
+        color: #005b96;
+      }
+    </style>
+  </head>
+  <body>
+    <h2>Infocenter</h2>
+    <p class="emphasis">PILGRIM Security Ltda.</p>
+
+    <p>
+      Carrera 13 No 94ª 26 Oficina 302<br />
+      Bogotá, Colombia<br />
+      <strong>Pbx:</strong> +60 (1) 7560730 Ext. 111<br />
+      <strong>Cel:</strong> +57 316 3696420 - 315 5449993<br />
+      <strong>Línea de emergencia fijo:</strong> 7560903<br />
+      <strong>Email:</strong>
+      <a href="mailto:centrodeoperaciones@pilgrimsecurity.net">
+        centrodeoperaciones@pilgrimsecurity.net
+      </a>
+    </p>
+
+    <p><em>No ofrecemos servicios, brindamos soluciones a nuestros clientes.</em></p>
+    <p><em>We don't offer services, we provide solutions to our clients.</em></p>
+
+    <div class="footer">
+      <p><strong>PRIVACIDAD DE DATOS:</strong> Te informamos que tus datos personales serán tratados por Pilgrim Security Ltda. conforme a lo previsto en la Ley 1581 de 2012. El tipo de tratamiento, la finalidad de este y los derechos que te asisten pueden ser consultados en nuestra política de tratamiento de datos personales disponible en <a href="https://www.pilgrimsecurity.net">www.pilgrimsecurity.net</a>.</p>
+
+      <p><strong>AVISO DE CONFIDENCIALIDAD:</strong> Este mensaje es privado y confidencial. Si lo recibiste por error, por favor notifícanos y elimínalo de tu sistema.</p>
+    </div>
+  </body>
+</html>",
      };
 
   try {
