bbded64a6dadd922d78567b6e8060980bb4272b3
jhonye73 <jespitia@umariana.edu.co>
2025-05-12 18:40:43 -0500

    reaparar.js

--- reaparar.js
+++ reaparar.js
@@ -29,26 +29,52 @@ app.use((req, res, next) => {
 
 app.post("/", async (req, res) => {
   const { correos, subject, text, html } = req.body;
-  const firmaHtml = `
-  <hr />
-  <p><strong>PILGRIM Security Ltda.</strong><br />
-  Carrera 13 No 94ª 26 Oficina 302, Bogotá, Colombia<br />
-  Pbx: +60 (1) 7560730 Ext. 111<br />
-  Cel: +57 3163696420 - 3155449993<br />
-  Línea de emergencia fijo: 7560903<br />
-  E-mail: <a href="mailto:centrodeoperaciones@pilgrimsecurity.net">centrodeoperaciones@pilgrimsecurity.net</a>
-  </p>
-  <p><em>No ofrecemos servicios, brindamos soluciones a nuestros clientes.</em><br />
-  <em>We don't offer services, we provide solutions to our clients.</em></p>
-  <p><strong>Privacidad:</strong> Tus datos serán tratados según la Ley 1581 de 2012. Más info en <a href="https://www.pilgrimsecurity.net">www.pilgrimsecurity.net</a>.</p>
-  <p><strong>Confidencialidad:</strong> Este mensaje es privado. Si lo recibiste por error, notifícanos y elimínalo.</p>
+  const firmaHtml =`
+    <h2>Infocenter</h2>
+    <p class="emphasis">PILGRIM Security Ltda.</p>
+
+    <p>
+      Carrera 13 No 94ª 26 Oficina 302<br />
+      Bogotá, Colombia<br />
+      <strong>Pbx:</strong> +60 (1) 7560730 Ext. 111<br />
+      <strong>Cel:</strong> +57 316 3696420 - 315 5449993<br />
+      <strong>Línea de emergencia fijo:</strong> 7560903<br />
+      <strong>Email:</strong>
+      <a href="mailto:centrodeoperaciones@pilgrimsecurity.net">
+        centrodeoperaciones@pilgrimsecurity.net
+      </a>
+    </p>
+
+    <p><em>No ofrecemos servicios, brindamos soluciones a nuestros clientes.</em></p>
+    <p><em>We don't offer services, we provide solutions to our clients.</em></p>
+
+    <div class="footer">
+      <p><strong>PRIVACIDAD DE DATOS:</strong> Te informamos que tus datos personales serán tratados por Pilgrim Security Ltda. conforme a lo previsto en la Ley 1581 de 2012. El tipo de tratamiento, la finalidad de este y los derechos que te asisten pueden ser consultados en nuestra política de tratamiento de datos personales disponible en <a href="https://www.pilgrimsecurity.net">www.pilgrimsecurity.net</a>.</p>
+
+      <p><strong>AVISO DE CONFIDENCIALIDAD:</strong> Este mensaje es privado y confidencial. Si lo recibiste por error, por favor notifícanos y elimínalo de tu sistema.</p>
 `;
 
 const htmlFinal = `
-  <html>
-    <head>
-      <meta charset="UTF-8" />
-    </head>
+<html>
+  <head>
+    <meta charset="UTF-8" />
+    <style>
+      body {
+        font-family: Arial, sans-serif;
+        color: #333;
+        line-height: 1.5;
+      }
+      .footer {
+        margin-top: 30px;
+        font-size: 12px;
+        color: #666;
+      }
+      .emphasis {
+        font-weight: bold;
+        color: #005b96;
+      }
+    </style>
+  </head>
     <body>
       ${html}
       ${firmaHtml}
